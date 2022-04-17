d60ebc7d0e986584cbfbc76e1ef1c73bdcf0d81e
ZeinMansor <zeinmanhasan90@gmail.com>
2022-04-17 22:28:01 +0300

    Zein, added smooth scroll to the nav bar

---
+++ src/components/About/AboutComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container
+        class="py-8 px-6"
+        fluid
+        id="About" 
+      >
+        <v-row>
+          <v-col
+            v-for="card in cards"
+            :key="card"
+            cols="12"
+          >
+            <v-subheader>{{ card }}</v-subheader>
+
+            <v-list two-line>
+              <template v-for="n in 6">
+                <v-list-item
+                  :id="n"
+                  :key="n"
+                >
+                  <v-list-item-avatar color="grey darken-1">
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title>Message {{ n }}</v-list-item-title>
+                    <v-list-item-subtitle>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider
+                  v-if="n !== 6"
+                  :key="`divider-${n}`"
+                  inset
+                ></v-divider>
+              </template>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
+
+<script>
+export default {
+    name: "AboutComponent",
+    data: function () {
+      return {
+        cards: ['Someday'],
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-app id="inspire">
+    
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      width="300"
+      class="ma-0"
+    >
+      <v-sheet
+        color="grey lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="i in icons"
+          :key="i.id"
+          link 
+          class="my-4"
+          :href="i.link"
+          @click="$vuetify.goTo(i.link , options)"
+        >
+          <router-link style="text-decoration: none; color: inherit;" :to="i.link">
+
+          <v-row>
+          
+          <v-list-item-icon >
+            <v-icon
+              :size="i.size"
+              :href="i.link"
+              color="blue lighten-2"
+            >{{ i.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ i.name }}</v-list-item-title>
+          </v-list-item-content>
+          
+          </v-row>
+          </router-link>
+        
+        </v-list-item>
+      
+      </v-list>
+    </v-navigation-drawer>
+      
+    <v-main>
+
+      <LandingComponent />
+      <AboutComponent />
+
+    </v-main>
+  </v-app>
+</template>
+
+
+
+
+
+
+<script>
+import LandingComponent from "../components/Landing/LandingComponent.vue";
+import AboutComponent from "../components/About/AboutComponent.vue"
+  export default {
+    name: "MainPage",
+    components: {
+      LandingComponent,
+      AboutComponent
+    },
+    data: function() {
+      return {
+        cards: ['Today', 'Yesterday'],
+        drawer: null,  
+          icons: [
+            {
+              id: '1',
+              name: 'Home',
+              icon: 'mdi-home',
+              link: '#home',
+              size: '30'
+            },
+
+            {
+              id: '2',
+              name: 'About',
+              icon: 'mdi-music-clef-treble',
+              link: '#About',
+              size: '30'
+            },
+
+            {
+              id: '6',
+              name: 'Education',
+              icon: 'mdi-library',
+              link: '/',
+              size: '30'
+            },
+
+            {
+              id: '3',
+              name: 'Resume',
+              icon: 'mdi-account-box',
+              link: '/',
+              size: '30'
+            },
+
+            {
+              id: '4',
+              name: 'Projects',
+              icon: 'mdi-home',
+              link: '/',
+              size: '30'
+            },
+
+            {
+              id: '5',
+              name: 'Contact',
+              icon: 'mdi-card-account-phone',
+              link: '/',
+              size: '30'
+            }
+
+          ]
+        }
+    }, 
+    computed : {
+      options () {
+        return {
+          duration: 1000,
+          offset: -20,
+          easing: 'easeInOutCubic',
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
